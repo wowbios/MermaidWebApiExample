e8c2419c9997627142dc31242acf83a611e5fe7c
wowbios <wowbios@yandex.ru>
2022-05-18 23:23:28 +0300

    init

---
+++ MermaidWeb/ClientApp/src/components/MMaid.js
@@ -0,0 +1,49 @@
+﻿import React, {Component, createRef} from 'react';
+import mermaid from 'mermaid';
+
+export class MMAid extends Component {
+    static displayName = MMAid.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { text: '', svg: null };
+        this.r = createRef();
+    }
+    
+    componentDidMount() {
+        mermaid.initialize({startOnLoad: false});
+    }
+    
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const node = this.r.current
+        if (node)
+        {
+            node.removeAttribute('data-processed')
+            mermaid.init(undefined, node);
+        }        
+    }
+
+    fetchDiagram = async () => {
+        const response = await fetch('mermaid')
+        const text = await response.text()
+        this.setState({text})
+    }
+    
+    onLoadClick = async () => {
+        await this.fetchDiagram()
+    }
+
+    render() {
+        const { text } = this.state
+        
+        return (
+            <>
+                Here is a mermaid diagram:
+                <button onClick={this.onLoadClick}>Load</button>
+                <div ref={this.r} id="mermaid" className="mermaid">
+                    {text}
+                </div>
+            </>
+        );
+    }
+}
---
+++ MermaidWeb/Controllers/MermaidController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MermaidWeb.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MermaidController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        string result = @"classDiagram
+      Animal <|-- Duck
+      Animal <|-- Fish
+      Animal <|-- Zebra
+      Animal : +int age
+      Animal : +String gender
+      Animal : +String male
+      Animal: +isMammal()
+      Animal: +mate()
+      class Duck{
+          +String beakColor
+          +swim()
+          +quack()
+      }
+      class Fish{
+          -int sizeInFeet
+          -canEat()
+      }
+      class Zebra{
+          +bool is_wild
+          +run()
+      }";
+        result += "\nAnimal:+int Height" + (char)Random.Shared.Next(0, 500);
+
+        return result;
+    }
+}
